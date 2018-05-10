a47154a0a080780624f4d936264824b917f873e7
Vassilis Xanthopoulos <xanthopoulos.vassilis@gmail.com>
2018-05-10 15:09:13 +0300

    Created Customer, Employee models; Updated spec

--- src/models/Customer.php
+++ src/models/Customer.php
@@ -6,10 +6,11 @@ class Customer extends Model {
 
     public $cust_IRS, $SSN, $first_name, $last_name, $first_registration;
     
-    protected $mapper = [
+    protected static $mapper = [
         'Customer_IRS' => ['cust_IRS', 'int'],
         'Social_Security_Number' => ['SSN', 'int'],
-        'Last_name' => ['last_name', 'strg'],
+        'First_Name' => 'first_name',
+        'Last_Name' => 'last_name',
         'First_Registration' => 'first_registration'
     ];
 
