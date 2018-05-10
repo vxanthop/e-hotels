f4364b1fe886aa291ec15eb47321bdca1ac910ae
Nick Loukas <nickloukas@outlook.com>
2018-05-10 13:59:20 +0300

    Created ofHotelGroup and manager_getter method in Hotel model; Created transaction model.

---
+++ src/models/Transaction.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace models;
+
+class Transaction extends Model {
+
+    public $transaction_id, $cust_IRS, $emp_IRS, $room_id, $hotel_id, $payment_amount, $payment_method;
+
+    protected $mapper = [
+        'Payment_Transaction_ID' => ['transaction_id', 'int'],
+        'Hotel_ID' => ['hotel_id', 'int'],
+        'Room_ID' => ['room_id', 'int'],
+        'Employee_IRS' => ['emp_IRS', 'int'],
+        'Customer_IRS' => ['cust_IRS', 'int'],
+        'Payment_Amount' => ['payment_amount', 'float'],
+        'Payment_Method' => 'payment_method'
+    ];
+
+
+}
