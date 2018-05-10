f866be2fde4761eddf26623719f2c218b418a129
Vassilis Xanthopoulos <xanthopoulos.vassilis@gmail.com>
2018-05-10 13:34:51 +0300

    Updated spec

---
+++ src/models/Customer.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace models;
+
+class Customer extends Model {
+
+    public $cust_IRS, $SSN, $first_name, $last_name, $first_registration;
+    
+    protected $mapper = [
+        'Customer_IRS' => ['cust_IRS', 'int'],
+        'Social_Security_Number' => ['SSN', 'int'],
+        'Last_name' => ['last_name', 'strg'],
+        'First_Registration' => 'first_registration'
+    ];
+
+    public function address_getter() {
+        return $this->address = [
+            'street' => $this->Address_Street,
+            'number' => $this->Address_Number,
+            'postal_code' => $this->Address_Postal_code,
+            'city' => $this->Address_City
+        ];
+    }
+
+}
