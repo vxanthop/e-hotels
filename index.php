2478ca71b2de8bad2af020cacf677fab6d8c625a
Theo Diamantidis <diamaltho@gmail.com>
2018-05-10 12:35:49 +0300

    Transferred query parameter handling to searchController from router

--- index.php
+++ index.php
@@ -29,19 +29,7 @@ $app->get('/', function () use ($app) {
 
 $app->get('/search', function () use ($app) {
 
-	$data = searchController::query([
-		'city' => $_GET['city'],
-		'start_date' => $_GET['start_date'],
-		'end_date' => $_GET['end_date'],
-		'capacity' => $_GET['capacity'],
-		'hotel_groups' => $_GET['hotel_groups'] ?? [],
-		'stars' => $_GET['stars'] ?? 0,
-		'price_start' => $_GET['price_start'] ?? 1,
-		'price_end' => $_GET['price_end'] ?? 300,
-		'rooms_start' => $_GET['rooms_start'] ?? 1,
-		'rooms_end' => $_GET['rooms_end'] ?? 100,
-		'amenities' => $_GET['amenities'] ?? [],
-	]);
+	$data = searchController::query($_GET);
 	return $app->Response('query.php', array_merge(
 		$data,
 		['_layout' => 'main.php']
