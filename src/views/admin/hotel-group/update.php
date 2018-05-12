63c131a45ea0a54df2ff1a6901c0f15510e20052
Theo Diamantidis <diamaltho@gmail.com>
2018-05-13 01:26:55 +0300

    Update view for Hotel groups

--- src/views/admin/hotel-group/update.php
+++ src/views/admin/hotel-group/update.php
@@ -3,37 +3,45 @@
                 <nav aria-label="breadcrumb">
                     <ol class="breadcrumb">
                         <li class="breadcrumb-item"><a href="/admin">Dashboard</a></li>
-                        <li class="breadcrumb-item active">Add Hotel Group</li>
+                        <li class="breadcrumb-item active">Edit Hotel Group: <?= $group->name ?></li>
                     </ol>
                 </nav>
                 <div class="container w-50 mt-5 mx-auto">
-                    <form action="/admin/hotel-group/createSubmit" method="POST">
+                    <form action="/admin/hotel-group/updateSubmit" method="POST">
                         <div class="form-group">
                             <label for="name">Hotel group name:</label>
-                            <input type="text" autofocus class="form-control" maxlength="42" required id="name" name="name" />
+                            <input type="text" autofocus class="form-control" maxlength="42" required id="name" name="name" value="<?= $group->name ?>" />
                         </div>
                         <div class="form-group row">
                             <div class="col">
                                 <label for="street">Address street:</label>
-                                <input type="text" class="form-control" maxlength="42" required id="street" name="street" />
+                                <input type="text" class="form-control" maxlength="42" required id="street" name="street" value="<?= $group->address['street'] ?>" />
                             </div>
                             <div class="col">
                                 <label for="number">Address number:</label>
-                                <input type="number" class="form-control" min="1" max="9999" required id="number" name="number" />
+                                <input type="number" class="form-control" min="1" max="9999" required id="number" name="number" value="<?= $group->address['number'] ?>" />
                             </div>
                         </div>
                         <div class="form-group row">
                             <div class="col">
                                 <label for="city">City:</label>
-                                <input type="text" class="form-control" maxlength="42" required id="city" name="city" />
+                                <input type="text" class="form-control" maxlength="42" required id="city" name="city" value="<?= $group->address['city'] ?>" />
                             </div>
                             <div class="col">
                                 <label for="postal">Postal code:</label>
-                                <input type="number" class="form-control" min="10000" max="999999" required id="postal" name="postal_code" />
+                                <input type="number" class="form-control" min="10000" max="999999" required id="postal" name="postal_code" value="<?= $group->address['postal_code'] ?>" />
                             </div>
                         </div>
                         <div class="form-group phones">
                             <label>Phone numbers:</label>
+                        <?php foreach($group->phone_numbers as $phone) { ?>
+                            <div class="input-group phone">
+                                <input type="number" class="form-control" name="phones[]" value="<?= $phone ?>" />
+                                <div class="input-group-append">
+                                    <button class="remove-input btn btn-secondary"><i class="fas fa-times"></i></button>
+                                </div>
+                            </div>
+                        <?php } ?>
                             <div class="input-group phone">
                                 <input type="number" class="form-control" name="phones[]" />
                                 <div class="input-group-append">
@@ -43,6 +51,14 @@
                         </div>
                         <div class="form-group emails">
                             <label>Email addresses:</label>
+                        <?php foreach($group->email_addresses as $email) { ?>
+                            <div class="input-group email">
+                                <input type="email" class="form-control" name="emails[]" value="<?= $email ?>" />
+                                <div class="input-group-append">
+                                    <button class="remove-input btn btn-secondary"><i class="fas fa-times"></i></button>
+                                </div>
+                            </div>
+                        <?php } ?>
                             <div class="input-group email">
                                 <input type="email" class="form-control" name="emails[]" />
                                 <div class="input-group-append">
@@ -68,6 +84,12 @@
                     input.value = ""
                     input.focus()
                     el.removeChild(btn.parentNode)
+                }
+                btn = event.target.closest(".remove-input")
+                if(btn !== null) {
+                    event.preventDefault()
+                    let el = btn.parentNode.parentNode
+                    el.parentNode.removeChild(el)
                 }
             })
         </script>
