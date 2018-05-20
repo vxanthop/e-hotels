8f6aaf992c9ebe794b2ca082eb391adc4c560375
Theo Diamantidis <diamaltho@gmail.com>
2018-05-21 01:23:56 +0300

    Fix query concatenation in URL model

--- src/models/URL.php
+++ src/models/URL.php
@@ -5,8 +5,13 @@ namespace models;
 class URL {
 
     public static function addQuery($url, $params) {
-        $p = parse_url($url); 
-        return $p['path'] . '?' . $p['query'] . http_build_query($params);
+        $p = parse_url($url);
+        if(strlen($p['query'])) {
+            $query = '?' . $p['query'] . '&' . http_build_query($params);
+        } else {
+            $query = '?' . http_build_query($params);
+        }
+        return $p['path'] . $query;
     }
 
 }
